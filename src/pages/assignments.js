5f3fe92abf2d995e44dae5c7a54fdfff27aa14ce
Massimo Guida <massimo.guidamusic@gmail.com>
2020-07-11 17:39:10 -0400

    corrected assignment rendering and added updated resume

--- src/pages/assignments.js
+++ src/pages/assignments.js
@@ -9,44 +9,69 @@ import Row from "../components/Row";
 import Col from "../components/Col";
 import CardAssignment3 from "../components/CardAssignment/CardAssignment3";
 import CardAssignment4 from "../components/CardAssignment/CardAssignment4";
+import CardAssignment8 from "../components/CardAssignment/CardAssignment8";
 import CardAssignment9 from "../components/CardAssignment/CardAssignment9";
 import CardAssignment10 from "../components/CardAssignment/CardAssignment10";
 import CardAssignment11 from "../components/CardAssignment/CardAssignment11";
 import CardAssignment12 from "../components/CardAssignment/CardAssignment12";
 import CardAssignment13 from "../components/CardAssignment/CardAssignment13";
 import CardAssignment19 from "../components/CardAssignment/CardAssignment19";
+import CardAssignment21 from "../components/CardAssignment/CardAssignment21";
+
 
 class Assignments extends Component {
   state = {
-    assignments: [
+    assignments:  [
       {
-        name: "Assignment #3: Password Generator",
-        description:
-          "A website built using JavaScript that allows users to generate a random secure password based on a number of selected criteria.",
-        technologies: ["HTML", "CSS", "JavaScript", "GitHub"],
-        url: "https://mg-cpu90.github.io/passwordgenerator/Develop/",
-        GitHub: "https://github.com/MG-cpu90/passwordgenerator",
+        "name": "Assignment #3: Password Generator",
+        "description": "A website built using JavaScript that allows users to generate a random secure password based on a number of selected criteria.",
+        "technologies": [
+          "HTML",
+          "CSS",
+          "JavaScript",
+          "GitHub"
+        ],
+        "url": "https://mg-cpu90.github.io/passwordgenerator/Develop/",
+        "GitHub": "https://github.com/MG-cpu90/passwordgenerator",
+        "id": "assignment3",
+        "img": "../../img/assignment3-ssh.png"
       },
       {
-        name: "Assignment #4: Coding Quiz",
-        description:
-          "An online quiz made using JavaScript and jQuery that allows users to test their basic general knowledge of coding.",
-        technologies: [
+        "name": "Assignment #4: Coding Quiz",
+        "description": "An online quiz made using JavaScript and jQuery that allows users to test their basic general knowledge of coding.",
+        "technologies": [
           "HTML",
           "CSS",
           "Bootstrap CSS Framework",
           "JavaScript",
           "jQuery",
-          "GitHub",
+          "GitHub"
         ],
-        url: "https://mg-cpu90.github.io/code-quiz/index.html",
-        GitHub: "https://github.com/MG-cpu90/code-quiz",
+        "url": "https://mg-cpu90.github.io/code-quiz/index.html",
+        "GitHub": "https://github.com/MG-cpu90/code-quiz",
+        "id": "assignment4",
+        "img": "../../img/assignment4-ssh.png"
       },
       {
-        name: "Assignment #9: ReadMe Generator",
-        description:
-          "A CLI application that prompts users to respond to some questions and subsequently generates a ReadMe file from a template which includes the information acquired from the users' responses.",
-        technologies: [
+        "name": "Assignment #8: Portfolio",
+        "description": "A front-end application containing a portfolio of my assignments and projects.",
+        "technologies": [
+          "HTML",
+          "CSS",
+          "Bootstrap CSS Framework",
+          "Font Awesome",
+          "Google Fonts",
+          "GitHub"
+        ],
+        "url": "https://mg-cpu90.github.io/code-quiz/index.html",
+        "GitHub": "https://github.com/MG-cpu90/assignment8",
+        "id": "assignment8",
+        "img": "../../img/assignment8-ssh.png"
+      },
+      {
+        "name": "Assignment #9: ReadMe Generator",
+        "description": "A CLI application that prompts users to respond to some questions and subsequently generates a ReadMe file from a template which includes the information acquired from the users' responses.",
+        "technologies": [
           "APIs",
           "JavaScript",
           "jQuery",
@@ -58,16 +83,17 @@ class Assignments extends Component {
           "Node.js",
           "Inquirer",
           "fs",
-          "GitHub",
+          "GitHub"
         ],
-        url: "",
-        GitHub: "https://github.com/MG-cpu90/assignment9",
+        "url": "#",
+        "GitHub": "https://github.com/MG-cpu90/assignment9",
+        "id": "assignment9",
+        "img": "https://media.giphy.com/media/XZaEKhpR8sVJa4l8yz/giphy.gif"
       },
       {
-        name: "Assignment #10: Template Engine – Employee Summary",
-        description:
-          "A CLI application that allows managers to generate a team html page by prompting them for a series of questions on professional information about themselves and their employees through node.",
-        technologies: [
+        "name": "Assignment #10: Template Engine – Employee Summary",
+        "description": "A CLI application that allows managers to generate a team html page by prompting them for a series of questions on professional information about themselves and their employees through node.",
+        "technologies": [
           "HTML",
           "CSS",
           "BootStrap",
@@ -84,16 +110,17 @@ class Assignments extends Component {
           "jest",
           "Inquirer",
           "fs",
-          "GitHub",
+          "GitHub"
         ],
-        url: "",
-        GitHub: "https://github.com/MG-cpu90/assignment10",
+        "url": "#",
+        "GitHub": "https://github.com/MG-cpu90/assignment10",
+        "id": "assignment10",
+        "img": "../../img/assignment10-ssh.png"
       },
       {
-        name: "Assignment #11: Note Taker",
-        description:
-          "A full-stack web application that allows users to create and save notes to a json file, or delete them.",
-        technologies: [
+        "name": "Assignment #11: Note Taker",
+        "description": "A full-stack web application that allows users to create and save notes to a json file, or delete them.",
+        "technologies": [
           "HTML",
           "CSS",
           "BootStrap",
@@ -109,16 +136,17 @@ class Assignments extends Component {
           "Express",
           "fs",
           "GitHub",
-          "Heroku",
+          "Heroku"
         ],
-        url: "https://floating-cliffs-26268.herokuapp.com/",
-        GitHub: "https://github.com/MG-cpu90/assignment11",
+        "url": "https://floating-cliffs-26268.herokuapp.com/",
+        "GitHub": "https://github.com/MG-cpu90/assignment11",
+        "id": "assignment11",
+        "img": "../../img/assignment11-ssh.png"
       },
       {
-        name: "Assignment #12: Employee Tracker",
-        description:
-          "A CLI application that allows users to organise their business by viewing, adding, or updating departemnts, roles, and employees in a company.",
-        technologies: [
+        "name": "Assignment #12: Employee Tracker",
+        "description": "A CLI application that allows users to organise their business by viewing, adding, or updating departemnts, roles, and employees in a company.",
+        "technologies": [
           "APIs",
           "JavaScript",
           "jQuery",
@@ -131,16 +159,17 @@ class Assignments extends Component {
           "Inquirer",
           "fs",
           "GitHub",
-          "Heroku",
+          "Heroku"
         ],
-        url: "",
-        GitHub: "https://github.com/MG-cpu90/assignment12",
+        "url": "#",
+        "GitHub": "https://github.com/MG-cpu90/assignment12",
+        "id": "assignment12",
+        "img": "../../img/assignment12-ssh.png"
       },
       {
-        name: "Assignment #13: Eat-Da-Burger!",
-        description:
-          'A restaurant application using node, express, and handlebars, that allows users to add burgers to the menu using a form, and subsequently to "devour" them using a button, which adds them to the "Devoured Burgers" list.',
-        technologies: [
+        "name": "Assignment #13: Eat-Da-Burger!",
+        "description": "A restaurant application using node, express, and handlebars, that allows users to add burgers to the menu using a form, and subsequently to 'devour' them using a button, which adds them to the 'Devoured Burgers' list.",
+        "technologies": [
           "HTML",
           "CSS",
           "Bootstrap",
@@ -157,16 +186,17 @@ class Assignments extends Component {
           "Zoom",
           "JawsDB",
           "GitHub",
-          "Heroku",
+          "Heroku"
         ],
-        url: "https://whispering-sands-43076.herokuapp.com/",
-        GitHub: "https://github.com/MG-cpu90/assignment13",
+        "url": "https://whispering-sands-43076.herokuapp.com/",
+        "GitHub": "https://github.com/MG-cpu90/assignment13",
+        "id": "assignment13",
+        "img": "../../img/assignment13-ssh.png"
       },
       {
-        name: "Assignment #19: Employee Directory",
-        description:
-          "A front-end application built using react, that allows users to sort columns of a table of employees, and to filter employees by their name.",
-        technologies: [
+        "name": "Assignment #19: Employee Directory",
+        "description": "A front-end application built using react, that allows users to sort columns of a table of employees, and to filter employees by their name.",
+        "technologies": [
           "HTML",
           "CSS",
           "Bootstrap",
@@ -183,11 +213,39 @@ class Assignments extends Component {
           "Git/Terminal",
           "JawsDB",
           "GitHub",
-          "Netlify",
+          "Netlify"
         ],
-        url: "https://happy-aryabhata-281606.netlify.app/",
-        GitHub: "https://github.com/MG-cpu90/assignment19",
+        "url": "https://happy-aryabhata-281606.netlify.app/",
+        "GitHub": "https://github.com/MG-cpu90/assignment19",
+        "id": "assignment19",
+        "img": "../../img/assignment19-ssh.png"
       },
+      {
+        "name": "Assignment #21: React Google Books Search",
+        "description": "A MERN application built using react, that allows users to search for books through google, add them to or fremove them from a reading list, and view information about them.",
+        "technologies": [
+          "HTML",
+          "CSS",
+          "Bootstrap",
+          "Font Awesome",
+          "JSX",
+          "React",
+          "JSON",
+          "Web Manifest",
+          "Service Worker",
+          "Visual Studio Code",
+          "Google Chrome",
+          "Node.js",
+          "Git/Terminal",
+          "MongoDB",
+          "GitHub",
+          "Heroku"
+        ],
+        "url": "https://reactgoogle-booksearch.herokuapp.com/",
+        "GitHub": "https://github.com/MG-cpu90/assignment21",
+        "id": "assignment21",
+        "img": "../../img/assignment21-ssh.png"
+      }
     ],
     cards: [
       {
@@ -222,6 +280,8 @@ class Assignments extends Component {
       return <CardAssignment3 />;
     } else if (this.state.currentCard === "assignment4") {
       return <CardAssignment4 />;
+    } else if (this.state.currentCard === "assignment8") {
+      return <CardAssignment8 />;
     } else if (this.state.currentCard === "assignment9") {
       return <CardAssignment9 />;
     } else if (this.state.currentCard === "assignment10") {
@@ -234,6 +294,8 @@ class Assignments extends Component {
       return <CardAssignment13 />;
     } else if (this.state.currentCard === "assignment19") {
       return <CardAssignment19 />;
+    } else if (this.state.currentCard === "assignment21") {
+      return <CardAssignment21 />;
     } else {
       return null;
     }
@@ -311,6 +373,32 @@ class Assignments extends Component {
                         </h5>
                       </a>
                       <p className="text-center">jQuery Coding Quiz</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col mb-4">
+                  <div className="card">
+                    <img
+                      src="../img/assignment8-ssh.png"
+                      className="card-img-top"
+                      alt="assignment8-screen_shot"
+                      width="200"
+                      height="200"
+                    ></img>
+                    <div className="card-body justify-content-center">
+                      <a
+                        className="nav-link"
+                        href="#"
+                        onClick={this.handleAssignmentRender}
+                      >
+                        <h5
+                          className="card-title btn-secondary text-white text-center p-2"
+                          id="assignment8"
+                        >
+                          Assignment 8
+                        </h5>
+                      </a>
+                      <p className="text-center">Portfolio</p>
                     </div>
                   </div>
                 </div>
@@ -469,6 +557,32 @@ class Assignments extends Component {
                         </h5>
                       </a>
                       <p className="text-center">Employee Directory</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col mb-4">
+                  <div className="card">
+                    <img
+                      src="../img/assignment21-ssh.png"
+                      className="card-img-top"
+                      alt="assignment21-screen_shot"
+                      width="200"
+                      height="200"
+                    ></img>
+                    <div className="card-body justify-content-center">
+                      <a
+                        className="nav-link"
+                        href="#"
+                        onClick={this.handleAssignmentRender}
+                      >
+                        <h5
+                          className="card-title btn-secondary text-white text-center p-2"
+                          id="assignment21"
+                        >
+                          Assignment 21
+                        </h5>
+                      </a>
+                      <p className="text-center">React Google Books Search</p>
                     </div>
                   </div>
                 </div>
